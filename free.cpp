7ab1cb2efd3d85012e0ef3807272e4da679f0d49
Monika kumari <monikakumari4687@gmail.com>
2026-05-31 06:05:52 +0530

    previous

---
+++ free.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int *ptr = (int *)calloc(5, sizeof(int));
+    // Do some operations.....
+    for (int i = 0; i < 5; i++)
+        printf("%d ", ptr[i]);
+    // Free the memory after completing
+    // operations
+    free(ptr);
+    return 0;
+}
