97e8d464da750118848619833c0791a6cb086ca5
Monika kumari <monikakumari4687@gmail.com>
2026-05-31 06:04:51 +0530

    previous

---
+++ condi.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a = 3, b = 4;
+    // conditional operaor
+    int result = (a<b) ? b : a;
+    cout << "the greatest number is " << endl;
+    return 0;
+}
