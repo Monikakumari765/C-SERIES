ebd58785314091e31607348a07cf96c1d5dd7769
Monika kumari <monikakumari4687@gmail.com>
2026-09-06 19:17:25 +0530

    practice

---
+++ loopsandarray.cpp/perfect.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n=28, sum=0;
+    for(int i=1;i<n;i++)
+
+        if(n%i==0) sum+=i;
+
+
+    cout << (sum==n ? "Perfect" : "Not Perfect");
+    
+}
