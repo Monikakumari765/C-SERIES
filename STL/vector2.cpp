e16bea0f834cde1c0394719b5cd7602fa910c7b0
Monika kumari <monikakumari4687@gmail.com>
2026-09-12 13:36:42 +0530

    vector2

---
+++ STL/vector2.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+    vector<int>vec;
+    vec.push_back(1);
+    vec.push_back(2);
+    vec.push_back(3);
+    vec.push_back(4);
+    vec.push_back(5);
+
+    for( int  val : vec){
+        cout << val << " ";
+    }
+    cout << endl;
+
+
+    //  cout << vec.size() << endl; // 3
+    //  cout << vec.capacity() << endl; // 4    
+
+    return 0;
+}
