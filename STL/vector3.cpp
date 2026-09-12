d701d1a51f36b9b1fd10a370697f9481ca180611
Monika kumari <monikakumari4687@gmail.com>
2026-09-12 14:15:38 +0530

    practice

--- STL/vector3.cpp
+++ STL/vector3.cpp
@@ -4,9 +4,13 @@ using namespace std;
 int main(){
 // vector<int>vec = {1,2,3,4,5}; // output is 1 2 3 4 5
 
-vector<int> vec(4,10); // 4 is the size and 10 is the value 
+//vector<int> vec(4,10); // 4 is the size and 10 is the value 
+//also used in dynamic programming 
 
-for(int val:vec){
+vector<int> vec1 = {1,2,3,4,5};
+vector<int>vec2(vec1);
+
+for(int val:vec1){
     cout<< val << " ";
 }
     cout << endl;
