da31db3a49b31ca73128d8a1ddd831f559c8e1d3
Monika kumari <monikakumari4687@gmail.com>
2026-09-12 12:52:13 +0530

    vector

--- STL/vector.cpp
+++ STL/vector.cpp
@@ -5,12 +5,18 @@ int main ()
 {
     vector<int>vec;  
     // 0
+    vec.push_back(1);
+    vec.push_back(2);
+     vec.push_back(3);
 
-    cout << vec.size() <<  endl;
-
+    cout << vec.size() <<  endl;  // 3
+    cout << vec.capacity() << endl; // 4
     return 0;
 }
 
+//  vec.push_back(1); == is used to change the value of vector from 0 to 1 
+// this is the property of vector  called size 
+// used to add the value 
 
 
-
+// size = 3 and capacity = 4 
