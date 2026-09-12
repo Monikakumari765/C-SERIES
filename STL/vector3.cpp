5b27641d9e5fd5d93c834aaad2a8a76e0e953f13
Monika kumari <monikakumari4687@gmail.com>
2026-09-12 14:11:11 +0530

    practice

--- STL/vector3.cpp
+++ STL/vector3.cpp
@@ -2,8 +2,9 @@
 #include<vector>
 using namespace std;
 int main(){
-vector<int>vec = {1,2,3,4,5};
+// vector<int>vec = {1,2,3,4,5}; // output is 1 2 3 4 5
 
+vector<int> vec(4,10); // 4 is the size and 10 is the value 
 
 for(int val:vec){
     cout<< val << " ";
