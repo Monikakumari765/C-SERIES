8a376843ac9bdfc22f74bad148054b69e11bc5cb
Monika kumari <monikakumari4687@gmail.com>
2026-09-13 08:49:05 +0530

    list1

---
+++ STL/list.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<vector>
+#include<list>
+using namespace std;
+int main()
+{
+    list<int> l;
+    l.push_back(1);
+    l.push_back(2);
+    l.push_front(3);
+    l.push_front(5);
+
+
+    //to print the element s we create for each loop
+   for(int val:l){
+    cout<< val << " " ;
+   }
+   cout << endl;
+    return 0;
+}
