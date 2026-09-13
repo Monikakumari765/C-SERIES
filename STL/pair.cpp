6ed8c0ca3976ac6a607abc29899ab91a125f5416
Monika kumari <monikakumari4687@gmail.com>
2026-09-13 09:32:20 +0530

    pair

---
+++ STL/pair.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+#include<list>
+#include<deque>
+using namespace std;
+int main(){
+// pair<int,int>p ={3,5}; output 1 5
+//pair<string, string > p= {"monika","gourav"};//monika gourav
+pair<int , char> p ={9,'a'}; //9 a
+
+cout<<p.first<<endl;
+cout<<p.second<<endl;
+
+return 0;
+
+}
