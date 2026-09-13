fe5e4739971c935cbddbb94eabd38cab4825bfb4
Monika kumari <monikakumari4687@gmail.com>
2026-09-13 19:42:38 +0530

    map

--- STL/map.cpp
+++ STL/map.cpp
@@ -8,11 +8,18 @@ int main()
     m["laptop"] = 50;
     m["remote"] = 46; // always in shorted in ascending order 
 
+
+    //m.insert({"camera", 9}); // camera 9
+    m.emplace("camera" , 9); //camera 9
+   m.erase("tv"); // to earase completely 
+
     for(auto p:m){
         cout<< p.first << " ";
         cout<< p.second << " ";
         cout<< endl;
 
     }
+    cout << "cout = " <<m.count("laptop") <<endl;// represent the no of that value which is present 
+
     return 0;
 }
