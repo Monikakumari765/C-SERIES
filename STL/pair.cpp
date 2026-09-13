89fb4736fe194a210b1a68d0b0466e7305901910
Monika kumari <monikakumari4687@gmail.com>
2026-09-13 09:48:01 +0530

    pair2

--- STL/pair.cpp
+++ STL/pair.cpp
@@ -6,11 +6,16 @@ using namespace std;
 int main(){
 // pair<int,int>p ={3,5}; output 1 5
 //pair<string, string > p= {"monika","gourav"};//monika gourav
-pair<int , char> p ={9,'a'}; //9 a
+//pair<int , char> p ={9,'a'}; //9 a
 
-cout<<p.first<<endl;
-cout<<p.second<<endl;
+ pair<int,pair<string , int>> p = {1 ,{"mona" ,3}} ;
 
+ cout<<p.first<<endl; //1
+ cout<<p.second.first<<endl; //mona 
+ cout<<p.second.second<<endl; //3
 return 0;
 
 }
+// pair<int,pair<string , int>> p = {1 ,{"mona" ,3}} ;
+// this is pair inside pair basically we used pair inside pair.
+
