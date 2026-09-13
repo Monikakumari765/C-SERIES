7e93dbe542555a3c8a608d613fda7bff1ca655b4
Monika kumari <monikakumari4687@gmail.com>
2026-09-13 08:58:54 +0530

    practice

--- STL/list.cpp
+++ STL/list.cpp
@@ -5,11 +5,13 @@ using namespace std;
 int main()
 {
     list<int> l;
-    l.push_back(1);
+    l.push_back(1); // emplace_back give us the same result 
     l.push_back(2);
     l.push_front(3);
     l.push_front(5);
 
+    l.pop_back();// to delete element form back
+    l.pop_front(); // to delete element form front 
 
     //to print the element s we create for each loop
    for(int val:l){
