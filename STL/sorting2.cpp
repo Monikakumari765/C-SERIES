583d5bb733013500f28560bd2d8b21406ea06058
Monika kumari <monikakumari4687@gmail.com>
2026-09-14 12:20:49 +0530

    sorting

--- STL/sorting2.cpp
+++ STL/sorting2.cpp
@@ -1,15 +1,26 @@
 #include<iostream>
 using namespace std;
+bool copmparator(pair<int, int >p1 , pair<int, int >p2){
+    if(p1.second< p2.second) return true;
+     if(p1.second > p2.second) return false;
+
+     if(p1.first<p2.second)return true;
+     else return false; // for sorting both
+}
+
 int main(){
-    vector<int> vec { 3,4 ,8 ,5 ,9};
+    vector<pair<int ,int>> vec= {{1,2},{3,4},{5,6},{7,8}};
 
 sort(vec.begin() , vec.end());
 
-for(int val:vec){
-    cout<< val << " ";
+for(auto p:vec){
+    cout<< p.first << " " <<p.second << endl;
 }
-cout<< endl;
-return 0;
 
+return 0;
+//1 2
+// 3 4
+// 5 6
+// 7 8 we get this value on the basics of thr first value 
 
 }
