88119a73eb36c35dac94c5344d6915bd4ee32009
Monika kumari <monikakumari4687@gmail.com>
2026-09-14 11:44:44 +0530

    sorting

---
+++ STL/sorting.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main(){
+  
+//   int arr[5] = {1 ,2 ,3 ,4,5 };
+//   sort(arr , arr +5);
+//   for(int val:arr){
+//     cout<<val <<" ";
+
+//   }
+//   cout<<endl;
+//   return 0;
+
+vector<int> vec { 3,4 ,8 ,5 ,9};
+
+sort(vec.begin() , vec.end());
+
+for(int val:vec){
+    cout<< val << " ";
+}
+cout<< endl;
+return 0;
+//3 4 5  8 9 
+
+}
